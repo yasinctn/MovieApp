09172d4087355bbe67c8ef67da874a2c648b14b3
yasinctin <yasin.cetin@marti.tech>
2025-08-05 09:39:14 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Yasin Çetin on 5.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
